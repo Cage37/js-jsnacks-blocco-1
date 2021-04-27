28ba777709cc6d1edb8b6234114a4ad689a47433
Cage37 <casiraghi.smn@gmail.com>
2021-04-27 12:31:45 +0200

    Task complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,16 +18,32 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti
 
-var number1 = Number(prompt("Inserisci un numero"));
-var number2 = Number(prompt("Inserisci un numero"));
-var number3 = Number(prompt("Inserisci un numero"));
-var number4 = Number(prompt("Inserisci un numero"));
-var number5 = Number(prompt("Inserisci un numero"));
-var number6 = Number(prompt("Inserisci un numero"));
-var number7 = Number(prompt("Inserisci un numero"));
-var number8 = Number(prompt("Inserisci un numero"));
-var number9 = Number(prompt("Inserisci un numero"));
-var number10 = Number(prompt("Inserisci un numero"));
-var somma = (number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10);
+// var number1 = Number(prompt("Inserisci un numero"));
+// var number2 = Number(prompt("Inserisci un numero"));
+// var number3 = Number(prompt("Inserisci un numero"));
+// var number4 = Number(prompt("Inserisci un numero"));
+// var number5 = Number(prompt("Inserisci un numero"));
+// var number6 = Number(prompt("Inserisci un numero"));
+// var number7 = Number(prompt("Inserisci un numero"));
+// var number8 = Number(prompt("Inserisci un numero"));
+// var number9 = Number(prompt("Inserisci un numero"));
+// var number10 = Number(prompt("Inserisci un numero"));
+// var somma = (number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10);
 
-console.log(somma);
+// console.log(somma);
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+var user = prompt("Come ti chiami?");
+var userList = ["Fabio", "Simone", "Sara", "Beatrice", "Sergio", "Anna", "Giovanni"];
+var ulLen = userList.length
+var access = "Non puoi entrare"
+
+for (var i = 0; i < ulLen; i++){
+    console.log(userList[i]);
+    if (user == userList[i]){
+        access = "Ottimo, puoi entrare";
+    }
+}
+
+console.log(access);
