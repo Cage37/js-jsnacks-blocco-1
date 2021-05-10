502794488aafdbe24e84b5154987bd88f27373b3
Cage37 <casiraghi.smn@gmail.com>
2021-05-10 12:16:28 +0200

    Task complete

--- blocco-4/main.js
+++ blocco-4/main.js
@@ -1,14 +1,32 @@
-// Creare un oggetto palla che abbia le seguenti proprietà: nome=palla, peso=10;
+// // Creare un oggetto palla che abbia le seguenti proprietà: nome=palla, peso=10;
 
-var palla = {
-    nome: "palla",
-    peso: 10
-};
+// var palla = {
+//     nome: "palla",
+//     peso: 10
+// };
 
-console.log(palla);
+// // Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
 
-// Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+// var nuovoPeso = Number(prompt("Inserisci un nuovo valore del peso"));
 
-var userPrompt = Number(prompt("Inserisci un nuovo valore del peso"));
+// palla.peso = nuovoPeso;
 
-palla.peso = userPrompt;
+// console.log(palla);
+
+
+// Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+// Calcolare perimetro e area.
+
+var triangolo = {
+    base: 30,
+    altezza: 40
+}
+
+var ipotenusa = Math.sqrt(Math.pow(triangolo.base, 2) + Math.pow(triangolo.altezza, 2));
+console.log(ipotenusa);
+
+var perimetro = ipotenusa + (triangolo.base * 2);
+console.log(perimetro);
+
+var area = (ipotenusa * triangolo.altezza) / 2;
+console.log(area);
